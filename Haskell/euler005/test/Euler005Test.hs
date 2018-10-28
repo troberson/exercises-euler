ca84b95457ec30586d004c29e88e0a7d98e98125
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 14:16:23 -0700

    Haskell: Euler 005: Improve Tests

--- Haskell/euler005/test/Euler005Test.hs
+++ Haskell/euler005/test/Euler005Test.hs
@@ -7,11 +7,13 @@ unitTests :: TestTree
 unitTests = testGroup "Euler - 005: Unit Tests"
   [ testCase
       "The smallest number that can be divided by each of the numbers from \
-      \1 to 10 without any remainder (2520)" $ lcmList [1..10] @?= 2520
+      \1 to 10 without any remainder is 2520" $
+      lcmList [1..10] @?= 2520
 
   , testCase
       "Final Answer: The smallest positive number that is evenly divisible by \
-      \all of the numbers from 1 to 20 (232792560)" $ euler005 @?= 232792560
+      \all of the numbers from 1 to 20 is 232792560" $
+      euler005 @?= 232792560
   ]
 
 main :: IO()
