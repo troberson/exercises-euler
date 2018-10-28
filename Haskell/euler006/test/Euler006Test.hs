a00792fe19a4cbf124ea1ef9fcbc3af451523bfe
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 14:19:27 -0700

    Haskell: Euler 006: Improve Tests

--- Haskell/euler006/test/Euler006Test.hs
+++ Haskell/euler006/test/Euler006Test.hs
@@ -6,21 +6,22 @@ import Euler006
 unitTests :: TestTree
 unitTests = testGroup "Euler - 006: Unit Tests"
   [ testCase
-      "Sum of the squares of the first ten natural numbers (385)" $
+      "The sum of the squares of the first ten natural numbers is 385" $
       sumOfSquares [1..10] @?= 385
 
   , testCase
-      "Square of the sum of the first ten natural numbers (3025)" $
+      "The square of the sum of the first ten natural numbers is 3025" $
       squareOfSum [1..10] @?= 3025
 
   , testCase
-      "Difference between the sum of the squares of the first ten natural \
-      \numbers and the square of the sum (2640)" $
+      "The difference between the sum of the squares of the first ten natural \
+      \numbers and the square of the sum is 2640" $
       sumSquareDifference [1..10] @?= 2640
 
   , testCase
-      "Final Answer: Difference between the sum of the squares of the first \
-      \one hundred natural numbers and the square of the sum (25164150)" $
+      "Final Answer: The difference between the sum of the squares of the \
+      \first one hundred natural numbers and the square of the sum is \
+      \25164150" $
       euler006 @?= 25164150
   ]
 
