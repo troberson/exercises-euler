9474f9fa6933671239978747cc942fb85d70314d
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 13:59:14 -0700

    Haskell: Template: Adjust Formatting

--- Haskell/template/test/EulerXXXTest.hs
+++ Haskell/template/test/EulerXXXTest.hs
@@ -5,8 +5,13 @@ import EulerXXX
 
 unitTests :: TestTree
 unitTests = testGroup "Euler - XXX: Unit Tests"
-  [ testCase "Foo ()" $ id 0 @?= 0
---  testCase "Final Answer ()" $ eulerXXX @?= 0
+  [ testCase
+      "Foo" $
+      id 0 @?= 0
+
+--  , testCase
+--      "Final Answer:" $
+--      eulerXXX @?= 0
   ]
 
 main :: IO()
