306acfccd064edca4def98eb6599cff669ae470e
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 14:23:02 -0700

    Haskell: Euler 008: Improve Tests

--- Haskell/euler008/test/Euler008Test.hs
+++ Haskell/euler008/test/Euler008Test.hs
@@ -8,7 +8,7 @@ unitTests = testGroup "Euler - 008: Unit Tests"
   [ testCase
       "The four adjacent digits in the 1000-digit number that have the \
       \greatest product are 9 × 9 × 8 × 9 = 5832" $
-        maxProductOfSlicesInSeries 4 @?= 5832
+      maxProductOfSlicesInSeries 4 @?= 5832
 
   , testCase
       "Final Answer: The product of the thirteen adjacent digits in the \
