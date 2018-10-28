1e782908e11e440e7e2df5f01ef65164dcc18fad
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 14:09:50 -0700

    Haskell: Euler 002: Improve Tests

--- Haskell/euler002/test/Euler002Test.hs
+++ Haskell/euler002/test/Euler002Test.hs
@@ -6,14 +6,14 @@ import Euler002
 unitTests :: TestTree
 unitTests = testGroup "Euler - 002: Unit Tests"
   [ testCase
-      "First ten Fibonacci numbers should be: \
-      \1, 2, 3, 5, 8, 13, 21, 34, 55, 89" $
+      "The first ten Fibonacci numbers are \
+      \1, 2, 3, 5, 8, 13, 21, 34, 55 and 89" $
       take 10 fibs @?=
       [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
   , testCase
-      "Final Answer: Sum of even-valued Fibonacci numbers up to \
-      \4,000,000 (4613732)" $
+      "Final Answer: The sum of the even-valued Fibonacci numbers whose values \
+      \do not exceed four million is 4613732" $
       euler002 @?= 4613732
   ]
 
