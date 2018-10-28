92346bcd603b68f84e6796167b0c34fe0ae45465
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-28 14:14:45 -0700

    Haskell: Euler 004: Improve Tests

--- Haskell/euler004/test/Euler004Test.hs
+++ Haskell/euler004/test/Euler004Test.hs
@@ -5,14 +5,22 @@ import Euler004
 
 unitTests :: TestTree
 unitTests = testGroup "Euler - 004: Unit Tests"
-  [ testCase "9009 is a Palindrome (True)" $ isPalindrome 9009 @?= True
-  , testCase "1234 is not a palindrome (False)" $ isPalindrome 1234 @?= False
+  [ testCase
+      "9009 is a palindrome" $
+      isPalindrome 9009 @?= True
+
   , testCase
-      "Largest palindrome made from the product of two 2-digit numbers (9009)" $
+      "1234 is not a palindrome" $
+      isPalindrome 1234 @?= False
+
+  , testCase
+      "The largest palindrome made from the product of two 2-digit numbers \
+      \ is 9009" $
       largestPalindromeProduct [10..99] @?= 9009
+
   , testCase
-      "Final Answer: Largest palindrome made from the product of two 3-digit \
-      \numbers (906609)" $
+      "Final Answer: The largest palindrome made from the product of two \
+      \3-digit numbers is 906609" $
       euler004 @?= 906609
   ]
 
