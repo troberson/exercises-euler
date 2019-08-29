c756d64d2f8985d94e9e2704453355359f3f3462
Tamara Roberson <tamara.roberson@gmail.com>
2019-08-28 20:40:06 -0700

    Euler 003 (C++) - Factors

---
+++ CXX/include/euler/utils/factors.hpp
@@ -0,0 +1,11 @@
+/*
+ * Project Euler - Utils - factors
+ * Tamara Roberson <tamara.roberson@gmail.com>
+ * Copyright (c) 2019 Tamara Roberson
+ *
+ * Functions for finding factors.
+ */
+
+#pragma once
+
+#include "factors.tpp"
---
+++ CXX/tests/utils_tests/factors_test.cpp
@@ -0,0 +1,56 @@
+/*
+ * Project Euler - Utils - factors (Tests)
+ * Tamara Roberson <tamara.roberson@gmail.com>
+ * Copyright (c) 2019 Tamara Roberson
+ *
+ * Functions for finding factors.
+ */
+
+#include <euler/utils/factors.hpp>
+
+#include <vector>
+
+#include <catch2/catch.hpp>
+
+// factor(n)
+TEST_CASE("Factors of 24")
+{
+    std::vector<int> facts = { 1, 2, 3, 4, 6, 8, 12, 24 };
+    REQUIRE(factor(24) == facts);
+}
+
+TEST_CASE("Factors of -8")
+{
+    std::vector<int> facts = { -8, -4, -2, -1, 1, 2, 4, 8 };
+    REQUIRE(factor(-8) == facts);
+}
+
+TEST_CASE("Factors of 0")
+{
+    REQUIRE(factor(0) == std::vector<int> { 0 });
+}
+
+TEST_CASE("Factors of 10, including negatives")
+{
+    std::vector<int> facts = { -10, -5, -2, -1, 1, 2, 5, 10 };
+    REQUIRE(factor(10, false, true) == facts);
+}
+
+TEST_CASE("Prime factors of 21")
+{
+    std::vector<int> facts = { 3, 7 };
+    REQUIRE(factor(21, true) == facts);
+}
+
+TEST_CASE("Prime factors of -1")
+{
+    REQUIRE(factor(-1, true).empty());
+}
+
+TEST_CASE("Prime Factors of 600851475143")
+{
+    auto n = 600851475143;
+    std::vector<decltype(n)> facts = {
+        71, 839, 1471, 6857 };
+    REQUIRE(factor(n, true) == facts);
+}
