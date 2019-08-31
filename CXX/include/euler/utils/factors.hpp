63ac6d46a4148f3b07e41813832e3c32dc3bf008
Tamara Roberson <tamara.roberson@gmail.com>
2019-08-31 13:46:10 -0700

    (C++) Eliminate separate template files
    There does not seem to be a good reason to keep the definitions of
    templated functions in separate .tpp files. The definitions are now
    simply in the header files.

--- CXX/include/euler/utils/factors.hpp
+++ CXX/include/euler/utils/factors.hpp
@@ -8,4 +8,119 @@
 
 #pragma once
 
-#include "factors.tpp"
+#include "primes.hpp"
+#include "template_types.hpp"
+
+#include <algorithm> // std::sort, std::transform
+#include <functional> // std::negate
+#include <vector> // std::vector
+
+
+/**
+ * Find the factors of a number
+ *
+ * @note `includeNegative` is always true if `n` is negative
+ *
+ * @param n the number to factor
+ * @param prime only look for prime factors (default: false)
+ * @param includeNegative include negative numbers (default: false)
+ * @returns the factors
+ */
+template <typename T, typename = IsIntegral<T>>
+std::vector<T> factor(T n, bool prime = false, bool includeNegative = false)
+{
+    // There are no prime factors of a negative number, 0, or 1
+    if (prime && n <= 1)
+    {
+        return {};
+    }
+
+    // Technically, zero has infinite factors but in lieu of that,
+    // we just return { 0 }.
+    if (n == 0)
+    {
+        return { 0 };
+    }
+
+    // If we are factoring a negative number, we must include negative
+    // factors. This overrides the parameter.
+    if (n < 0)
+    {
+        includeNegative = true;
+        n = -n; // for all other purposes, n and -n are the same
+    }
+
+    // Find all positive factors.
+    std::vector<T> facts;
+
+
+    // Calculate the factors.
+    //
+    // Every time we find a factor, we record it and its complement
+    // For prime factors, we are looking for only for the first
+    // prime numbers whose product is the original number, so we divide
+    // the number by the factor each time until we reach 1
+
+    // The maximum number considered is calculated differently for
+    // prime factors and regular factors.
+    T max = n;
+    for (T i = 1; i <= max; i++)
+    {
+        // Not a factor, continue
+        if (n % i != 0)
+        {
+            continue;
+        }
+
+        // Prime factor check
+        if (prime)
+        {
+            // Factor isn't prime, continue
+            if (!isPrime(i))
+            {
+                continue;
+            }
+
+            // Add the factor to the list
+            facts.push_back(i);
+
+            // Only consider the numbers needed to
+            // complete the product
+            max /= i;
+        }
+        else // Regular factor check
+        {
+            // Add the factor to the list
+            facts.push_back(i);
+
+            // Also add its complement
+            facts.push_back(n/i);
+
+            // Only consider numbers towards the center (n/2)
+            // Subtract 1 so we don't count the same factor twice
+            max = n/i - 1;
+        }
+    }
+
+    // Sort the list of factors
+    // The regular factor check records numbers out of order
+    std::sort(facts.begin(), facts.end());
+
+    // The negative factors are the same as the positive factors,
+    // so we just mirror the positive ones in a combined list
+    if (includeNegative)
+    {
+        // Negate all factors into a new list
+        std::vector<T> factsNeg;
+        std::transform(facts.rbegin(), facts.rend(),
+            std::back_inserter(factsNeg), std::negate<T>());
+
+        // Return the two lists joined together
+        std::vector<T> factsPosNeg(factsNeg);
+        factsPosNeg.insert(factsPosNeg.end(), facts.begin(), facts.end());
+        return factsPosNeg;
+    }
+
+    // Return factors
+    return facts;
+}
--- CXX/include/euler/utils/primes.hpp
+++ CXX/include/euler/utils/primes.hpp
@@ -8,4 +8,90 @@
 
 #pragma once
 
-#include "primes.tpp"
+#include "template_types.hpp"
+
+#include <limits> // std::numeric_limits
+
+
+/**
+ * Check if a number is prime.
+ *
+ * Valid for any integral type (int, long, unsigned int, etc.).
+ * Uses trial division.
+ *
+ * @param n number to check
+ * @returns true if number is prime, false otherwise
+ */
+template <typename T, typename = IsIntegral<T>>
+bool isPrime(T n)
+{
+    // Negative numbers, 0, and 1 are not prime
+    if (n < 2)
+    {
+        return false;
+    }
+
+    // 2 is prime
+    if (n == 2)
+    {
+        return true;
+    }
+
+    // All other even numbers are not prime
+    if (n % 2 == 0)
+    {
+        return false;
+    }
+
+    // Test all odd numbers up to n/2
+    for (int i = 3; i < n/2; i += 2)
+    {
+        // Found a divisor, number is not prime
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    // Did not find a divisor, number is prime
+    return true;
+}
+
+
+/**
+ * Find next prime
+ *
+ * @param start number to start from
+ * @returns a prime number or 0 if max size of type is reached
+ */
+template <typename T, typename = IsIntegral<T>>
+T findNextPrime(T start)
+{
+    // The first prime number is 2
+    if (start < 2)
+    {
+        return 2;
+    }
+
+    // Start searching from the next number after the one
+    // we are given
+    start++;
+
+    // Start on an odd number, so we can only test odd numbers
+    if (start % 2 == 0)
+    {
+        start++;
+    }
+
+    // Search for the next prime number
+    T max = std::numeric_limits<T>::max();
+    for (int i = start; i <= max; i += 2)
+    {
+        if (isPrime(i))
+        {
+            return i;
+        }
+    }
+
+    // return 0 if we hit the maximum of the type without reaching another prime number
+    return 0;
+}
