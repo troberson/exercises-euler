f2e775bebdc6892dab4d4401f03158c1bbf654a2
Tamara Roberson <tamara.roberson@gmail.com>
2019-09-01 12:56:43 -0700

    (C++) Implement Euler 006

---
+++ CXX/include/euler/euler006.hpp
@@ -0,0 +1,61 @@
+/*
+ * Project Euler - 006 - Sum Square Difference
+ * Tamara Roberson <tamara.roberson@gmail.com>
+ * Copyright (c) 2019 Tamara Roberson
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *   1^2 + 2^2 + ... + 10^2 = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ *   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural
+ * numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ */
+
+
+namespace euler
+{
+
+/**
+ * Solves the first example problem.
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *   1^2 + 2^2 + ... + 10^2 = 385
+ *
+ * @returns the solution to the problem.
+ */
+int euler006_example1();
+
+
+/**
+ * Solves the second example problem.
+ *
+ * The square of the sum of the first ten natural numbers is,
+ *   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * @returns the solution to the problem.
+ */
+int euler006_example2();
+
+
+/**
+ * Solves the third example problem.
+ *
+ * Hence the difference between the sum of the squares of the first ten
+ * natural numbers and the square of the sum is 3025 − 385 = 2640.
+ */
+int euler006_example3();
+
+/**
+ * Solves the problem.
+ *
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ */
+int euler006();
+
+} // end namespace euler
---
+++ CXX/src/euler006.cpp
@@ -0,0 +1,63 @@
+/*
+ * Project Euler - 006 - Sum Square Difference
+ * Tamara Roberson <tamara.roberson@gmail.com>
+ * Copyright (c) 2019 Tamara Roberson
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *   1^2 + 2^2 + ... + 10^2 = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ *   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural
+ * numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ */
+
+#include <euler/euler006.hpp>
+#include <euler/utils/sum.hpp>
+
+
+namespace euler
+{
+
+int square_of_sums(int max)
+{
+    int sum = utils::sum(max);
+    return sum * sum;
+}
+
+
+int sum_square_difference(int max)
+{
+    return square_of_sums(max) - utils::sum_of_squares(max);
+}
+
+
+int euler006_example1()
+{
+    return utils::sum_of_squares(11); // 1-11 exclusive
+}
+
+
+int euler006_example2()
+{
+    return square_of_sums(11); // 1-11 exclusive
+}
+
+
+int euler006_example3()
+{
+    return sum_square_difference(11); // 1-11 exclusive
+}
+
+
+int euler006()
+{
+    return sum_square_difference(101); // 1-100 exclusive
+}
+
+} // end namespace euler
+
